6550f245b08a59ce920a5beedcfc4778e73c4e4b
Christina Takara <christina.takara@gmail.com>
2025-02-11 17:04:48 -0600

    updated cors

--- index.mjs
+++ index.mjs
@@ -24,6 +24,13 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
+
 // ROUTES
 app.get('/', (req, res) => {
     res.send(
